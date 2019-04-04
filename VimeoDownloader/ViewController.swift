50f6ae9d6d7adbef8ffd4c0206ddb1fb00f6e7ca
Alba Garza <agarza@multinfmty.com>
2019-04-04 11:59:42 -0600

    First comment!

--- VimeoDownloader/ViewController.swift
+++ VimeoDownloader/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var idVimeo: String? = nil
+    
+    @IBOutlet weak var imageTitle: UILabel!
+    @IBOutlet weak var idTextBar: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func downloadVimeo(_ sender: Any) {
+        idVimeo = nil
+        idVimeo = idTextBar.text
+        parseVimeoJason()
+    }
+    
+    
+    func parseVimeoJason(){
+        guard let url = URL(string: "https://player.vimeo.com/video/\(idVimeo!)/config") else {return}
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let dataResponse = data,
+                error == nil else {
+                    print(error?.localizedDescription ?? "Response Error")
+                    return }
+            do {
+                let json = try JSONSerialization.jsonObject(with: dataResponse, options: []) as! [String: AnyObject]
+                //print(type(of: json))
+                if let video = json["video"] as? [String:AnyObject]{
+                    if let title = video["title"] as? String{
+                        print(title)
+                        if let routes = json["request"] as? [String:AnyObject]{
+                            //print(type(of: routes))
+                            if let legs = routes["files"] as? [String:AnyObject]{
+                                //print(type(of: legs))
+                                if let steps = legs["progressive"] as? [[String:AnyObject]]{
+                                    //print(type(of: legs))
+                                    if let vimeoURL = steps[0]["url"] as? String{
+                                        print(vimeoURL)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            } catch let error as NSError {
+                print("Failed to load: \(error.localizedDescription)")
+            }
+        }
+        task.resume()
+    }
 }
-
